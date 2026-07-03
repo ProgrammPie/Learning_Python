d5e02cdbe8d0e3e747445a98bd4b71709148a10f
ProgrammPie <mystudingacc@gmail.com>
2026-07-04 01:46:46 +0300

    Прохождение первого модуля

---
+++ tests/zero_test.py
@@ -0,0 +1,9 @@
+from hello.main import greet
+
+
+def test_greet_basic() -> None:
+    assert greet("World") == "Hello, World!"
+
+
+def test_greet_empty() -> None:
+    assert greet("") == "Hello, !"
